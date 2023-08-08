e1801d54247dd15c3c9a5e864fcdf98cec014b34
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-08-08 14:20:32 -0400

    adds background component

---
+++ src/components/Background.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+
+function Background() {
+
+    return(
+        <>
+            <div className="bg-img1">
+                <Header />
+            </div>
+            <div className="bg-img"></div>
+            <div className="bg-img2"></div>
+            <div className="bg-img"></div>
+            <div className="bg-img3"></div>
+        </>
+    )
+}
+
+export default Background
